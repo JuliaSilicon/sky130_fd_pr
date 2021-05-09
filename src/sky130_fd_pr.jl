65e403df12e44d5d83e1f4bde99d37e3e36732bc
Keno Fischer <keno@juliacomputing.com>
2021-05-09 04:39:39 -0400

    Allow precompile of the dummy file

--- src/sky130_fd_pr.jl
+++ src/sky130_fd_pr.jl
@@ -1,5 +1,5 @@
 module sky130_fd_pr
 	# ERROR: TODO: Maybe have a julia-side SPICE model registry also,
 	# to avoid requiring explicit .lib in the `.sp` files?
-	error("This is a SPICE model package, not a julia package. Don't attempt to load it like this.")
+	__init__() = error("This is a SPICE model package, not a julia package. Don't attempt to load it like this.")
 end
