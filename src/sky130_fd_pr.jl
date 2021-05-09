115e32721f36c570ae2f9f34b665d836b0d4b3e0
Keno Fischer <keno@juliacomputing.com>
2021-05-09 04:33:07 -0400

    Add dummy julia file
    At the moment, the package manager complains if a package contains
    no loadable julia file. We may want to reconsider that, but for now
    this is fine.

---
+++ src/sky130_fd_pr.jl
@@ -0,0 +1,5 @@
+module sky130_fd_pr
+	# ERROR: TODO: Maybe have a julia-side SPICE model registry also,
+	# to avoid requiring explicit .lib in the `.sp` files?
+	error("This is a SPICE model package, not a julia package. Don't attempt to load it like this.")
+end
